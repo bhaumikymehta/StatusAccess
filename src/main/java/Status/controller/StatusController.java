3ebab9fca34d61920184a3442869bc39bbf86b40
Bhaumik Mehta <bhaumikymehta@gmail.com>
2018-09-03 14:48:09 -0500

    Swagger configuration and some other changes

--- src/main/java/Status/controller/StatusController.java
+++ src/main/java/Status/controller/StatusController.java
@@ -1,14 +1,20 @@
 package Status.controller;
 
+import io.swagger.annotations.Api;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@Api(value = "Status controller",description = "Description for status codes")
+@RequestMapping("/status")
 public class StatusController {
 
-    @RequestMapping("/")
+    @RequestMapping(value = "/",method = RequestMethod.GET)
     public String index(){
         StringBuilder builder =new StringBuilder();
         Map<String,String> map=System.getenv();
@@ -22,8 +28,12 @@ public class StatusController {
         System.out.println(builder);
         return builder.toString();
     }
-    @RequestMapping("/gls")
+    @RequestMapping(value="/gls",method = RequestMethod.GET)
     public String getGlsVersion(){
         return  "This is GLS project and the version is GLS 2.0";
+    }
+    @RequestMapping(value="/show",method = RequestMethod.GET)
+    public ResponseEntity showResponse(){
+        return  new ResponseEntity("Positive Response", HttpStatus.OK);
     }
 }
